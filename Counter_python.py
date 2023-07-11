d849e2775916823d51ceca744e58ab49bbbff989
Nico Rojas <rojasangelnicolas@gmail.com>
2023-07-11 20:54:38 -0300

    Crear la funcion que registra un cliente

--- Counter_python.py
+++ Counter_python.py
@@ -1,7 +1,18 @@
 from collections import Counter
+
+def crear_cliente():
+    cliente={}
+    cliente['nombre']=input("Ingrese su nombre: ")
+    cliente['apellido']=input("Ingrese su apellido: ")
+    return cliente
+
+
 #Estamos probando el paquete de modulos de Counter
-myList=[1,1,2,3,4,5,3,2,3,4,1,2,3]
+"""myList=[1,1,2,3,4,5,3,2,3,4,1,2,3]
 print(Counter(myList)) 
 
 x=input().split(" ")
-print(x)
+print(x) """
+
+cli=crear_cliente()
+print(cli)
