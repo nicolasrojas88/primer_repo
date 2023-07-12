1e2b981caea904f680eb068e7ced7ae3c75f00e9
Nico Rojas <rojasangelnicolas@gmail.com>
2023-07-11 21:03:03 -0300

    Borramos todo el codigo

--- Counter_python.py
+++ Counter_python.py
@@ -1,7 +1,2 @@
-from collections import Counter
-#Estamos probando el paquete de modulos de Counter
-myList=[1,1,2,3,4,5,3,2,3,4,1,2,3]
-print(Counter(myList)) 
-
-x=input().split(" ")
-print(x)
+nombre=input("Ingrese su nombre: ")
+print(f"Decime {nombre}¿Qué paso con todo el código?")
